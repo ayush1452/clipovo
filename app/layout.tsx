874c9043497061b36327a125b16ad07b7b6aa516
Ayush Sharma <ayush0810sharma@gmail.com>
2025-05-28 00:09:25 -0400

    deleted the line

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > Updated the line for the codebase
+      > Updated
         {children}
       </body>
     </html>
