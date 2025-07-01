dc9ffc61eae2b26f6b58a387e32cd617d030d667
Ayush Sharma <ayush0810sharma@gmail.com>
2025-07-01 23:38:53 +0530

    deleted unnecessary code

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > Update the codebase again for new changes
+      > Update
         {children}
       </body>
     </html>
