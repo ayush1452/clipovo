e9ddbf32a8761e8cd76ea314fae651ef2abcc54d
Ayush Sharma <ayush0810sharma@gmail.com>
2025-08-04 22:53:37 +0530

    new update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > fixed the change
+      > new update
         {children}
       </body>
     </html>
