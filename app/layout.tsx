eca370b19084d5281ec7066694a00e3f52e94215
Ayush Sharma <ayush0810sharma@gmail.com>
2025-08-18 21:47:13 +0530

    added the cron job

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > changes reverted
+      > new update
         {children}
       </body>
     </html>
