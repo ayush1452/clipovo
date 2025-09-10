c702b81361c67fb60fdaed3854c8f701dd110222
Ayush Sharma <ayush0810sharma@gmail.com>
2025-09-10 23:33:07 +0530

    index creation

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      > tested new queries
+      > tested new indexes creation
         {children}
       </body>
     </html>
