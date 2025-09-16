231d2b29d57c1e93cd150ceee86184b9861d1b5d
Ayush Sharma <ayush0810sharma@gmail.com>
2025-09-16 23:03:49 +0530

    test cases

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >  remove the slow query
+      >  added new test case
         {children}
       </body>
     </html>
