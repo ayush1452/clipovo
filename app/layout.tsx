a782da1123b5ae324e1b5adfe24e5d164bc9e805
Ayush Sharma <ayush0810sharma@gmail.com>
2025-09-18 22:28:00 +0530

    frontend change

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >  fixed query
+      >  added the switch button on the UI
         {children}
       </body>
     </html>
