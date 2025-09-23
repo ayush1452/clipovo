c049894d1306b04d5dd6b168884c874db3adc7b3
Ayush Sharma <ayush0810sharma@gmail.com>
2025-09-23 21:13:41 +0530

    refactored backend code

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >  added the frontend layout
+      >  refactored the old changes
         {children}
       </body>
     </html>
