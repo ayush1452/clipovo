8b628b6dbc6a96a0f2d845f7c7d24c1d041efa7d
Ayush Sharma <ayush0810sharma@gmail.com>
2025-10-06 03:09:34 +0530

    doc update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >  update a new api
+      >  added new doc update
         {children}
       </body>
     </html>
