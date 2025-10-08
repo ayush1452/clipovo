27081c6bfc0588217f87b1795dece02dfe3c9385
Ayush Sharma <ayush0810sharma@gmail.com>
2025-10-08 23:05:53 +0530

    fix failed api

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >  added new doc update
+      >  refactor rest api
         {children}
       </body>
     </html>
