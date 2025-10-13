b9afef7ff5c779d4f0dd8f12c8227b6bbcc6af32
Ayush Sharma <ayush0810sharma@gmail.com>
2025-10-13 23:32:40 +0530

    major issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >  new changes for updating the filtering in the code
+      >  fix added in prod
         {children}
       </body>
     </html>
