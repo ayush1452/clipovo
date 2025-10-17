4091bf0d590fd788903824aba2d971f9a802787f
Ayush Sharma <ayush0810sharma@gmail.com>
2025-10-17 21:48:21 +0530

    frontend fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >  fixed the slow queries issue
+      > issue with the frontend box
         {children}
       </body>
     </html>
